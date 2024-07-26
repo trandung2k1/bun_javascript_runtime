b1ec946aa6b0aac4187a9f79bb1fad19b0bfa59a
trandung2k1 <trandungitksnb2001@gmail.com>
2024-07-26 10:43:09 +0700

    update code and config

---
+++ app.ts
@@ -0,0 +1,3 @@
+import { sum } from './utils/sum';
+const rs = sum(1, 3);
+console.log(rs);
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+//Http server Bun
 // const server = Bun.serve({
 //   port: 3000,
 //   fetch(req) {
@@ -6,15 +7,17 @@
 // });s
 // console.log(`Listening on http://localhost:${server.port} ...`);
 
-import express from "express";
-
+import express from 'express';
+import { sum } from './utils/sum';
+const rs = sum(1, 2);
+console.log(rs);
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+    console.log(`Listening on port ${port}...`);
 });
